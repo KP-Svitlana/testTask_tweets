f65377d49ab931803ad9baad21e0a90373208d18
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-06-22 15:40:08 +0300

    update: userCard styles

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -4,6 +4,9 @@ import {
   StyledLogo,
   StyledImg,
   StyledElement,
+  StyledTextWrap,
+  StyledText,
+  StyledAvatar,
 } from "./UserCard.styled";
 
 import Logo_1x from "../../img/Logo_1x.png";
@@ -13,6 +16,8 @@ import Logo_3x from "../../img/Logo_3x.png";
 import userImg_1x from "../../img/userImg_1x.png";
 import userImg_2x from "../../img/userImg_2x.png";
 import userImg_3x from "../../img/userImg_3x.png";
+
+import ava2 from "../../img/ava2.png";
 
 export const UserCard = (isActive) => {
   return (
@@ -25,7 +30,13 @@ export const UserCard = (isActive) => {
         <source srcSet={`${userImg_1x} 1x,${userImg_2x} 2x,${userImg_3x} 3x`} />
         <img src={userImg_1x} alt="Logo GoIT" />
       </StyledImg>
+      <StyledAvatar src={`${ava2}`} />
       <StyledElement />
+      <StyledTextWrap>
+        <StyledText>777 tweets</StyledText>
+        <StyledText>100,501 Followers</StyledText>
+      </StyledTextWrap>
+
       <Button isActive={isActive} />
     </StyledUserCard>
   );
--- src/components/UserCard/UserCard.styled.js
+++ src/components/UserCard/UserCard.styled.js
@@ -21,8 +21,25 @@ export const StyledLogo = styled.picture`
 `;
 
 export const StyledImg = styled.picture`
+  position: relative;
+  top: -14px;
+  left: 0;
   margin: 0 auto;
-  margin-bottom: 18px;
+  margin-bottom: 4px;
+`;
+
+export const StyledAvatar = styled.img`
+  width: 64px;
+  height: 64px;
+  background: #ebd8ff;
+  border: 8px solid #ebd8ff;
+  border-radius: 50%;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+    inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+
+  position: absolute;
+  top: 186px;
+  left: 150px;
 `;
 
 export const StyledElement = styled.div`
@@ -30,4 +47,26 @@ export const StyledElement = styled.div`
   background: #ebd8ff;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
     inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+`;
+
+export const StyledTextWrap = styled.div`
+  padding: 26px 0;
+  margin: 0 auto;
+  margin-top: 36px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const StyledText = styled.p`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+
+  color: #ebd8ff;
 `;
